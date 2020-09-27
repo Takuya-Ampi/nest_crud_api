f33e4e9ce27ba6630c1c3b8d78ff352830eac7ad
行比拓也 <imac@iMac.local>
2020-09-27 13:33:29 +0900

    postsのgetリクエスト追加。dtoフォルダ移動

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,14 +1,18 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
-import { CreatePostDto } from '../dto/create-post.dto';
+import { CreatePostDto } from './dto/create-post.dto';
 import { PostsService } from './posts.service';
 import { Posts } from './post.entity';
 
-@Controller()
+@Controller('/posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
-  @Post('/posts')
+  @Post()
   create(@Body() createPostDto: CreatePostDto) {
     return this.postsService.create(createPostDto);
-}
+  }
+  @Get()
+  findAll(): Promise<Posts[]> {
+    return this.postsService.findAll();
+  }
 
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { CreatePostDto } from '../dto/create-post.dto';
+import { CreatePostDto } from './dto/create-post.dto';
 import { Repository } from 'typeorm';
 import { Posts } from './post.entity';
 
