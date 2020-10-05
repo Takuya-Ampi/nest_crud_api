ac76ed92c28c0f0f705132180aeb3f572b4fc4ee
行比拓也 <imac@iMac.local>
2020-10-05 16:27:54 +0900

    userにカスタムレポジトリ作成

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,20 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
-import { Repository } from 'typeorm';
+import {UserRepository} from "./user.custom.repository"
 import { User } from './user.entity';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User)
-    private readonly userRepository: Repository<User>,
+    private readonly userRepository: UserRepository,
   ) {}
   async create(createUserDto: CreateUserDto) {
     return await this.userRepository.save(createUserDto)
   }
   async findAll(): Promise<User[]> {
     return await this.userRepository.find()
+  }
+  async findByName(name: string) {
+    return await this.userRepository.findByName(name)
   }
 
 }
