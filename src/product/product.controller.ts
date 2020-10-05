269c029b35f169c392a62c87bb7725f13829e9e8
行比拓也 <imac@iMac.local>
2020-10-05 12:02:40 +0900

    コントローラー及びサービスの実装

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Get } from '@nestjs/common';
+import { ProductService } from './product.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { Product } from './product.entity';
 
 @Controller('products')
-export class ProductController {}
+export class ProductController {
+  constructor(private readonly productService: ProductService) {}
+  @Post()
+  create(@Body() createProductDto: CreateProductDto){
+    return this.productService.create(createProductDto)
+  }
+  @Get()
+  findAll(): Promise<Product[]> {
+    return this.productService.findAll()
+  }
+}
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from './product.entity';
+import { CreateProductDto } from './dto/create-product.dto';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>
+  ){}
+  async create(createProductDto: CreateProductDto) {
+    return  await this.productRepository.save(createProductDto)
+  }
+  async findAll(): Promise<Product[]> {
+    return await this.productRepository.find()
+  }
+
+}
